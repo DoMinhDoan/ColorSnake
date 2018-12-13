962745c6816310e4f78ebad89bd14116357b2bcf
doan.dominh <doan.dominh@gameloft.com>
2018-12-13 14:16:46 +0700

    - added Snake Color

---
+++ Assets/EndlessRun/Scripts/ColorPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPlayer : MonoBehaviour {
+    	
+    //If you use only2D object use OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Colors"))
+        {
+            print("OnTriggerEnter Colors\n");
+
+            Material mat = other.gameObject.GetComponent<MeshRenderer>().material;
+            gameObject.transform.parent.GetComponent<PlayerControler>().AddPlayerNode(mat);
+
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("Obstacles"))
+        {
+            print("OnTriggerEnter Obstacles\n");
+
+            Material mat = other.gameObject.GetComponent<MeshRenderer>().sharedMaterial;
+            if(gameObject.transform.parent.GetComponent<PlayerControler>().HitObstacle(mat, other.gameObject))
+            {
+                //Destroy(other.gameObject);
+            }
+            
+        }
+    }
+}
---
+++ Assets/EndlessRun/Scripts/PlayerControler.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+
+    public EndlessRun er;
+
+    public List<GameObject> players;
+    public GameObject playerSample;
+    public int maxPlayerCount = 5;
+
+    public int maxMovingStep = 1;
+    public int distanceMoving = 100;
+
+    float stepIntervalY = 1.0f;
+    float stepIntervalX = 0.02f;
+    int movingOffset = 0;
+
+    Vector3 touchPosWorld;
+
+    //Change me to change the touch phase used.
+    TouchPhase touchPhase = TouchPhase.Ended;
+
+
+    void Start()
+    {
+        AddPlayerNode(null);        
+    }
+
+    void Update()
+    {
+        //We check if we have more than one touch happening.
+        //We also check if the first touches phase is Ended (that the finger was lifted)
+        if ((Input.touchCount > 0 && Input.GetTouch(0).phase == touchPhase) || Input.GetMouseButtonDown(0))
+        {
+            Ray ray;
+            if (Input.touchCount > 0)   // ??? phone
+            {
+                ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            }
+            else
+            {
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            }
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {                
+                GameObject touchedObject = hit.transform.gameObject;
+                if(touchedObject.transform.CompareTag("Player"))
+                {
+                    GameObject first = players[0];
+                    players.RemoveAt(0);
+                    players.Add(first);
+
+                    UpdatePlayerTransformLocationY(0);
+                }                
+            }
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {           
+            if(movingOffset > -maxMovingStep)
+            {
+                movingOffset--;
+                transform.Translate(Vector3.left * distanceMoving * Time.deltaTime);
+            }
+            
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            if (movingOffset < maxMovingStep)
+            {
+                movingOffset++;
+                transform.Translate(Vector3.right * distanceMoving * Time.deltaTime);
+            }                
+        }
+
+        // update snake transform
+        if (stepIntervalY > 0 && players[0].transform.localPosition.y > 0)
+        {
+            UpdatePlayerTransformLocationY(stepIntervalY);
+
+            stepIntervalY -= 0.02f;
+        }
+        
+    }
+
+    void UpdatePlayerTransformLocationX(float interval)
+    {
+        for (int i = 0; i < players.Count; i++)
+        {
+            Vector3 localPos = new Vector3((-i / maxPlayerCount) + interval, players[i].transform.localPosition.y, 0);
+
+            players[i].transform.localPosition = localPos;
+        }
+    }
+
+    void UpdatePlayerTransformLocationY(float interval)
+    {
+        for (int i = 0; i < players.Count; i++)
+        {
+            Vector3 localPos = new Vector3(0, -i + interval, 0);
+
+            players[i].transform.localPosition = localPos;
+        }
+    }
+
+    public void AddPlayerNode(Material mat)
+    {
+        GameObject go = Instantiate(playerSample, new Vector3(0, players.Count, 0), Quaternion.identity, this.transform) as GameObject;
+
+        if(players.Count == 0)
+        {
+            go.transform.localPosition = new Vector3(0, 0, 0);
+        }
+
+        if(mat != null)
+        {
+            go.GetComponent<MeshRenderer>().material = mat;
+        }
+
+        if(players.Count == maxPlayerCount)
+        {
+            GameObject lastGO = players[maxPlayerCount - 1];
+            players.RemoveAt(maxPlayerCount - 1);
+            Destroy(lastGO);
+        }
+        players.Insert(0, go);       
+        stepIntervalY = 1.0f;
+    }
+
+    public bool HitObstacle(Material mat, GameObject obstacle)
+    {
+        if (players.Count > 1)
+        {
+            if( players[0].GetComponent<MeshRenderer>().sharedMaterial.name.Contains(mat.name))
+            {                
+                var toRemove = new List<GameObject>();
+                for(int i = 0; i < players.Count; i++)
+                {
+                    print("Obstacle Hit OK");
+                    if (players[i].GetComponent<MeshRenderer>().sharedMaterial.name.Contains(mat.name))
+                    {
+                        toRemove.Add(players[i]);
+                        Destroy(players[i].gameObject);
+                    }
+                    else
+                    {
+                        break;
+                    }                    
+                }
+
+                players.RemoveAll(toRemove.Contains);
+                if(players.Count == 0)
+                {
+                    er.isGameOver = true;
+                    return false;
+                }
+
+                Destroy(obstacle);
+                UpdatePlayerTransformLocationY(0);
+
+                return true;
+            }
+            else
+            {
+                er.isGameOver = true;
+                return false;
+            }
+        }
+        else if (players.Count == 1 && players[0].GetComponent<MeshRenderer>().sharedMaterial.name.Contains(mat.name))
+        {
+            Destroy(obstacle);
+            return true;
+        }
+
+        er.isGameOver = true;
+        return false;
+    }
+   
+}
